ccb62ebb629cce7de0005aa49da7da2c8f80b17f
amaduela <anselmomaduela@gmail.com>
2018-07-20 21:57:33 +0200

    demo

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var notify = require('gulp-notify');
+var cssnano = require('gulp-cssnano');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('bootstrap', function() {
+	return gulp.src('./node_modules/bootstrap/scss/**/**')
+	  .pipe(gulp.dest(('src/scss/bootstrap/')));
+});
+
+gulp.task('scss', function () {
+	gulp.src('src/scss/*.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass())
+		.pipe(cssnano())
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('listen', function() {
+	gulp.watch('src/scss/**/*.scss', ['scss']);
+	// gulp.watch('src/js/*.js', ['js']);
+});
